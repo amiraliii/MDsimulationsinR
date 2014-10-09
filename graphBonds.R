c3d8c637a911dde74c5123ba5dd83e4a7a0a18eb
jeremyNg <jeremy.ng.wk1990@gmail.com>
2014-10-10 01:37:05 +0800

    Commit code for plotting graph with graphviz; function will return a graphNEL object that needs a plot call

--- graphBonds.R
+++ graphBonds.R
@@ -1,16 +1,30 @@
 # function to plot the hbond network
 cutoff <- 0.5
+fn <- "hbonds/WTaverage_hbonds.out"
 plotGraph <- function(fn="hbonds/WTaverage_hbonds.out",
-                      cutoff=0.5){
+                      cutoff=0.7){
     require(Rgraphviz) # loads rgraphviz library
     networkfile <- read.table(file=fn,header = FALSE) # read in the file
     networkfile <- networkfile[networkfile$V5>cutoff,] # keeps only those above the cutoff value for plotting
-    acceptors <- unique(networkfile$V1) # keep the acceptors
-    donors <- unique(networkfile$V3) # keep the donors
-    nodes <- paste0("Residue",unique(c(acceptors,donors))) # this is the node list
+    acceptors <- as.vector(unique(networkfile$V1)) # keep the acceptors
+    donors <- as.vector(unique(networkfile$V3)) # keep the donors
+    nodes <- unique(c(acceptors,donors)) # this is the node list
     # generate the edge list
-    edgelist <- lapply(donors,function(x){w <- networkfile[networkfile$V3==x,];m <- w$V1;m <- as.vector(m);m <-paste0("Residue",m);m})
-    names(edgeli)
-    graph <- new("graphNEL", nodes=nodes, edgeL=edgelist, edgemode="undirected")
-    graph <- layoutGraph(graph)
+    edgelist <- lapply(donors,function(x){w <- networkfile[networkfile$V3==x,];m <- w$V1;m <- as.vector(m);m})
+    names(edgelist) <- donors
+    graph <- new("graphNEL", nodes=nodes, edgemode="directed")
+    for(j in 1:length(edgelist)){
+            graph <- addEdge(from=names(edgelist[j]),unlist(edgelist)[j],graph,1)
+            }
+    graph
     }
+        
+
+
+wtGraph <- plotGraph(cutoff=0.75)
+mutGraph <- plotGraph(fn="hbonds/Mutaverage_hbonds.out",cutoff=0.75)
+#png("hbonds/graphviz1.png")
+par(mfrow = c(2,1))
+plot(wtGraph,main="wt. hydrogen bond with cutoff 0.75")
+plot(mutGraph,main="Mut hydrogen bond with cutoff 0.75")
+#dev.off()
