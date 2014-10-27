9d1575050406de7fa22070cb48f3fcfa6ae86206
jeremyNg <jeremy.ng.wk1990@gmail.com>
2014-10-28 00:55:24 +0800

    modified .mcolnames to simplify code base

--- massignColNames.R
+++ massignColNames.R
@@ -1,7 +1,5 @@
-multiColnames <- function(...,cname){
+multiColnames <- function(objlist,cname){
     # reads in the argument names are the objects
-    objnames <- c(...)
-    # if all passes, then we go on with assignment of colnames
-    lapply(objnames,function(x) {obj <- get(x);colnames(obj) <- cname;assign(x=x,obj,envir = globalenv())})
+    lapply(objlist,function(x) {obj <- get(x);colnames(obj) <- cname;assign(x=x,obj,envir = globalenv())})
     return(invisible())
     }
